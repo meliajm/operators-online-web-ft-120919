6607e2d299821b56a1e344de6e611d479cf06538
melia miller <meliajm@gmail.com>
2019-12-09 19:37:12 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,8 +11,14 @@ end
 
 
 def not_safe?(speed)
-	speed > 60 ? true : false
 	speed < 40 ? true : false 
+	
+# 	speed > 60 ? true : false
+	elsif speed > 60
+    true
+  else 
+    false
+  end 
 end
 	
 
